5d52ec0d925859f4fcb93d87ebaa33f76006cb89
sarath <vssarathc04@gmail.com>
2022-05-17 11:06:34 +0530

    added constants file

---
+++ src/vtop_handler/constants.py
@@ -0,0 +1,17 @@
+VTOP_BASE_URL = r"https://vtop2.vitap.ac.in/vtop/"
+VTOP_LOGIN_URL = r"https://vtop2.vitap.ac.in/vtop/vtopLogin"
+VTOP_DO_LOGIN_URL = r"https://vtop2.vitap.ac.in/vtop/doLogin"
+VTOP_ATTENDANCE_URL = r"https://vtop2.vitap.ac.in/vtop/processViewStudentAttendance"
+VTOP_TIMETABLE_URL = r"https://vtop2.vitap.ac.in/vtop/processViewTimeTable"
+VTOP_ACADHISTORY_URL = r"https://vtop2.vitap.ac.in/vtop/examinations/examGradeView/StudentGradeHistory"
+VTOP_PROFILE_URL = r"https://vtop2.vitap.ac.in/vtop/studentsRecord/StudentProfileAllView"
+VTOP_MARKS_URL = r"https://vtop2.vitap.ac.in/vtop/examinations/doStudentMarkView"
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+}
+
+SEM_IDS = [
+    "AP2021225",
+    "AP2021226"
+]
