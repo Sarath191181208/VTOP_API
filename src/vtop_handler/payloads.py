0d4fb9518431a1ef0e249a3b4cd927f7783b5cce
sarath <vssarathc04@gmail.com>
2022-05-17 11:06:56 +0530

    added payloads

---
+++ src/vtop_handler/payloads.py
@@ -0,0 +1,33 @@
+"""
+
+    payloads for different requests
+
+"""
+
+import datetime
+
+get_vtop_profile_payload = lambda user_name:{
+    "verifyMenu" : "true",        
+    "winImage" : "undefined",
+    "authorizedID": user_name,
+    "nocache" : "@(new Date().getTime())"   
+}
+
+get_vtop_timetable_payload = lambda username, semID:{
+            "semesterSubId" : semID,      
+            "authorizedID" : username,
+            "x" : datetime.datetime.now(datetime.timezone.utc).strftime("%c GMT")   
+}
+
+get_vtop_attendance_payload = lambda username, semId : {
+        "semesterSubId" : semId,  
+        "authorizedID" : username,
+        "x" : datetime.datetime.now(datetime.timezone.utc).strftime("%c GMT")   # GMT time
+}
+
+get_vtop_academic_profile_payload = lambda username:{
+        "verifyMenu" : "true",        
+        "winImage" : "undefined",
+        "authorizedID": username,
+        "nocache" : "@(new Date().getTime())"   
+}
