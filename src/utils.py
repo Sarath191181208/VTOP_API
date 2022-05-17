db807fcf4249aade16fdab6da881a267c80ce42e
sarath <vssarathc04@gmail.com>
2022-05-17 11:47:11 +0530

    created colorama print for better visibility

---
+++ src/utils.py
@@ -0,0 +1,37 @@
+import colorama
+colorama.init()
+
+_get_color_from_str = lambda clr : {
+    'error': colorama.Fore.RED,
+    'warning': colorama.Fore.YELLOW,
+    'info': colorama.Fore.GREEN,
+    'debug': colorama.Fore.BLUE,
+    'red': colorama.Fore.RED,
+    'green': colorama.Fore.GREEN,
+    'yellow': colorama.Fore.YELLOW,
+    'blue': colorama.Fore.BLUE,
+    'magenta': colorama.Fore.MAGENTA,
+    'cyan': colorama.Fore.CYAN,
+    'white': colorama.Fore.WHITE,
+}.get(clr.lower(), colorama.Fore.WHITE)
+
+def c_print(*msg, **kwargs):
+    color = _get_color_from_str(kwargs.get('color', None))
+    end = kwargs.get('end', '\n')
+    if color:
+        print(color , msg , colorama.Style.RESET_ALL, end=end)
+    else:
+        print(msg)
+
+if __name__ == "__main__":
+    c_print('hello', color='red')
+    c_print('hello', color='green')
+    c_print('hello', color='yellow')
+    c_print('hello', color='blue')
+    c_print('hello', color='magenta')
+    c_print('hello', color='cyan')
+    c_print('hello', color='white')
+    c_print('hello', color='error')
+    c_print('hello', color='warning')
+    c_print('hello', color='info')
+    c_print('hello', color='debug')
