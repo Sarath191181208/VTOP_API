142b66613dc08d9a196128ae41ab6741c3e65802
Sarath <vssarathc04@gmail.com>
2023-03-03 23:11:55 +0530

    : semester names -> subject names

---
+++ src/vtop_handler/course_page/get_semester_names.py
@@ -0,0 +1,10 @@
+# get link
+# create a payload 
+# send a post request
+# get the response and parse 
+# return the parsed response
+
+from ..payloads import get_semeseter_names_payload
+
+def get_semester_names():
+    
