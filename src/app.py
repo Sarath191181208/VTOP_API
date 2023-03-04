2e19a9da9826ef031ca58574bada465efb6d6f58
Sarath <vssarathc04@gmail.com>
2023-03-04 18:06:50 +0530

    + may_throw, cookie session decorators

--- src/app.py
+++ src/app.py
@@ -14,11 +14,11 @@ from src.vtop_handler import get_timetable, get_attendance, get_acadhistory
 from src.vtop_handler.course_page_handler import get_course_semesters_list
 from src.vtop_handler import get_exam_schedule
 
-from src.validators import is_valid_username_password
+from src.validators import validate_username_password
 
-from src.vtop_handler.Exceptions import InvalidCredentialsException, BadRequestException
+from src.vtop_handler.Exceptions import CustomBaseException, InvalidCredentialsException, BadRequestException
 
-from time import time
+from src.decorators import is_cookie_present, may_throw
 
 PORT = 5000
 app = Flask(__name__)
@@ -29,7 +29,6 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 import logging
-from src.utils import c_print
 logging.basicConfig(filename='flask_logs.log', level=logging.DEBUG)
 
 def get_all_details_futures(sess: aiohttp.ClientSession, user_name: str):
@@ -50,74 +49,52 @@ def hello_world():
     return 'Hello World!'
 
 @app.route('/api/v1/alldetails', methods=['POST'])
+@may_throw
 async def all_details():
-    if request.method == 'POST':
-        try: 
-            user_name = request.form.get('username', None)
-            passwd = request.form.get('password', None)
 
-            if user_name is None or passwd is None or not is_valid_username_password(user_name, passwd):
-                raise InvalidCredentialsException(status_code=400)
+    user_name = request.form.get('username', "")
+    passwd = request.form.get('password', "")
 
-            async with aiohttp.ClientSession() as sess:
-                user_name = await generate_session(user_name,passwd, sess)
-                if user_name is None: raise InvalidCredentialsException(status_code=401)
-                all_details_futures = get_all_details_futures(sess, user_name)
-                # awaiting all details to arrive and converting to dict
-                all_detials = {
-                    k: (await d_future)[0]
-                    for k, d_future in all_details_futures.items()
-                }
-        except InvalidCredentialsException as ICexception:
-            return jsonify({"Error": ICexception.msg}), ICexception.status_code
-        except Exception as e:
-            logging.exception(e)
-            return jsonify({"Error": "Internal Server Error"}), 500
+    validate_username_password(user_name, passwd)
 
-        return jsonify(all_detials), 200
+    async with aiohttp.ClientSession() as sess:
+        user_name = await generate_session(user_name,passwd, sess)
+        if user_name is None: raise InvalidCredentialsException(status_code=401)
+        all_details_futures = get_all_details_futures(sess, user_name)
+        # awaiting all details to arrive and converting to dict
+        all_detials = {
+            k: (await d_future)[0]
+            for k, d_future in all_details_futures.items()
+        }
+    return jsonify(all_detials), 200
     
 @app.route('/api/v1/exam_schedule', methods=['POST'])
 async def exam_schedule():
-    if request.method == 'POST':
-        try: 
-            user_name = request.form.get('username', None)
-            passwd = request.form.get('password', None)
+    user_name = request.form.get('username', "")
+    passwd = request.form.get('password', "")
 
-            if user_name is None or passwd is None or not is_valid_username_password(user_name, passwd):
-                raise InvalidCredentialsException(status_code=400)
+    validate_username_password(user_name, passwd)
 
-            async with aiohttp.ClientSession() as sess:
-                user_name = await generate_session(user_name,passwd, sess)
-                if user_name is None: raise InvalidCredentialsException(status_code=401)
-                all_detials, is_valid = await get_exam_schedule(sess, user_name)
+    async with aiohttp.ClientSession() as sess:
+        user_name = await generate_session(user_name,passwd, sess)
+        if user_name is None: raise InvalidCredentialsException(status_code=401)
+        all_detials, is_valid = await get_exam_schedule(sess, user_name)
 
-        except InvalidCredentialsException as ICexception:
-            return jsonify({"Error": ICexception.msg}), ICexception.status_code
-        except Exception as e:
-            logging.exception(e)
-            return jsonify({"Error": "Internal Server Error"}), 500
-
-        return jsonify(all_detials), 200
+    return jsonify(all_detials), 200
 
 @app.route('/api/v1/login', methods=['POST'])
+@may_throw
 async def login():
     cookie = None
-    try: 
-        user_name = request.form.get('username', None)
-        passwd = request.form.get('password', None)
+    user_name = request.form.get('username', "")
+    passwd = request.form.get('password', "")
 
-        if user_name is None or passwd is None or not is_valid_username_password(user_name, passwd):
-            raise InvalidCredentialsException(status_code=400)
-        # extract cookie from vtop
-        async with aiohttp.ClientSession() as sess:
-            user_name = await generate_session(user_name,passwd, sess)
-            cookie = sess.cookie_jar.filter_cookies('https://vtop2.vitap.ac.in/vtop').get('JSESSIONID').value # type: ignore
-            if user_name is None: raise InvalidCredentialsException(status_code=401)
-    except InvalidCredentialsException as ICexception:
-        return jsonify({"Error": ICexception.msg}), ICexception.status_code
-    except Exception as e:
-        logging.exception(e)
-        return jsonify({"Error": "Internal Server Error"}), 500
+    validate_username_password(user_name, passwd)
+    # extract cookie from vtop
+    async with aiohttp.ClientSession() as sess:
+        user_name = await generate_session(user_name,passwd, sess)
+        cookie = sess.cookie_jar.filter_cookies('https://vtop2.vitap.ac.in/vtop').get('JSESSIONID').value # type: ignore
+        if user_name is None: raise InvalidCredentialsException(status_code=401)
     
     session["cookie"] = cookie
     return jsonify({"cookie": cookie}), 200
---
+++ src/decorators.py
@@ -0,0 +1,28 @@
+from functools import wraps
+import logging
+from flask import jsonify, session
+
+from src.vtop_handler.Exceptions.custom_base_exception import CustomBaseException
+
+
+def is_cookie_present(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        cookie = session.get("cookie", None)
+        if cookie is None:
+            return jsonify({"Error": "You must login to access this route! "}), 401
+        return await func(*args, **kwargs)
+    return wrapper
+
+
+def may_throw(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        try:
+            return await func(*args, **kwargs)
+        except CustomBaseException as exception:
+            return jsonify({"Error": exception.msg}), exception.status_code
+        except Exception as e:
+            logging.exception(e)
+            return jsonify({"Error": "Internal Server Error"}), 500
+    return wrapper
