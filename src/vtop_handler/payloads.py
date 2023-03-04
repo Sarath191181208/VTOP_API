4852706cc2b5a8f8d526a88477ca6d4c4089367e
Sarath <vssarathc04@gmail.com>
2023-03-04 10:09:34 +0530

    + get_course_page_table_of_contents_payload

--- src/vtop_handler/payloads.py
+++ src/vtop_handler/payloads.py
@@ -36,16 +36,25 @@ get_exam_schedule_payload = lambda username, semId : {
         "authorizedID" : username,
 }
 
-get_course_page_semeseter_names_payload = lambda roll_no :  {
+get_course_page_semeseter_names_payload = lambda auth_id :  {
         "verifyMenu": True,
         "winImage": None,
-        "authorizedID": roll_no,
+        "authorizedID": auth_id,
         "nocache": "@(new Date().getTime())"
 }
 
-get_course_page_subject_names_payload = lambda sub_sem_id, roll_no: {
+get_course_page_subject_names_payload = lambda sub_sem_id, auth_id: {
         "paramReturnId": "getCourseForCoursePage",
         "semSubId": sub_sem_id,
-        "authorizedID": roll_no,
-        "x": "Fri, 03 Mar 2023 17:01:29 GMT"
+        "authorizedID": auth_id,
+        "x": get_curr_time_vtop_format()
+}
+
+get_course_page_table_of_contents_payload = lambda class_id, sem_id, auth_id:{
+        "classId": class_id,
+        "praType": "source",
+        "paramReturnId": "getSlotIdForCoursePage",
+        "semSubId": sem_id,
+        "authorizedID": auth_id, 
+        "x": get_curr_time_vtop_format()
 }
