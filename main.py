d35210b46f6188012c53ac0567e8e0834e6b2948
Sarath <vssarathc04@gmail.com>
2023-03-05 08:40:35 +0530

    changed prot_get_env from str -> int

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.app import app
 import os 
 
-PORT = os.getenv("PORT", 5050) 
+PORT = os.getenv("PORT", 5000) 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=PORT)
+    app.run(host='0.0.0.0', port=int(PORT))
