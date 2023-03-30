28bd55be02ba36226d66364d59c9a8c117ac1a39
Sarath <vssarathc04@gmail.com>
2023-03-30 07:15:10 +0530

    + testing to the start script

--- start.sh
+++ start.sh
@@ -1,10 +1,17 @@
 # run mypy on the src folder 
 mypy src
 
+# install the vtop_handler package 
+python -m pip install -e .
+
 # run pytest on the tests folder
 pytest tests
+
+# uninstall the vtop_handler package 
+python -m pip uninstall vtop_handler -y
 
 # start the server
 gunicorn --bind 0.0.0.0:5050 wsgi:app --reload
 
+# recording the usage statistics
 # psrecord --plot memory.png "python main.py" 
