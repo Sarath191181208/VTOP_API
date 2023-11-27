4f4e2c4b6049517e0acc8eb12c7762e45389782c
Sarath <vssarathc04@gmail.com>
2023-11-27 16:43:14 +0530

    fmt of tests

--- tests/user_names_passwords.py
+++ tests/user_names_passwords.py
@@ -1,34 +1,42 @@
-from functools import wraps
 import os
 from typing import Any, List, Tuple, Union
 import dotenv
-import pytest
+
 dotenv.load_dotenv()
 
-USERNAME = os.getenv('VTOP_USERNAME_1', "")
-PASSWORD = os.getenv('VTOP_PASSWORD_1', "")
-VITEEE_USERNAME_1 = os.getenv('VITEEE_USERNAME_1', "")
-VITEEE_PASSWORD_1 = os.getenv('VITEEE_PASSWORD_1', "")
-FRESHER_USERNAME_1 = os.getenv('FRESHER_USERNAME_1', "")
-FRESHER_PASSWORD_1 = os.getenv('FRESHER_PASSWORD_1', "")
-VTOP_USERNAME_2 = os.getenv('VTOP_USERNAME_2', "")
-VTOP_PASSWORD_2 = os.getenv('VTOP_PASSWORD_2', "")
+USERNAME = os.getenv("VTOP_USERNAME_1", "")
+PASSWORD = os.getenv("VTOP_PASSWORD_1", "")
+VITEEE_USERNAME_1 = os.getenv("VITEEE_USERNAME_1", "")
+VITEEE_PASSWORD_1 = os.getenv("VITEEE_PASSWORD_1", "")
+FRESHER_USERNAME_1 = os.getenv("FRESHER_USERNAME_1", "")
+FRESHER_PASSWORD_1 = os.getenv("FRESHER_PASSWORD_1", "")
+VTOP_USERNAME_2 = os.getenv("VTOP_USERNAME_2", "")
+VTOP_PASSWORD_2 = os.getenv("VTOP_PASSWORD_2", "")
 
-for i in [USERNAME, PASSWORD,
-          VITEEE_USERNAME_1, VITEEE_PASSWORD_1,
-          FRESHER_USERNAME_1, FRESHER_PASSWORD_1, VTOP_USERNAME_2, VTOP_PASSWORD_2]:
+for i in [
+    USERNAME,
+    PASSWORD,
+    VITEEE_USERNAME_1,
+    VITEEE_PASSWORD_1,
+    FRESHER_USERNAME_1,
+    FRESHER_PASSWORD_1,
+    VTOP_USERNAME_2,
+    VTOP_PASSWORD_2,
+]:
     assert i != "", "Please set the environment variables"
 
 username_password_list: List[Tuple[str, str]] = [
     (USERNAME, PASSWORD),
     (VITEEE_USERNAME_1, VITEEE_PASSWORD_1),
     (FRESHER_USERNAME_1, FRESHER_PASSWORD_1),
-    (VTOP_USERNAME_2, VTOP_PASSWORD_2)
+    (VTOP_USERNAME_2, VTOP_PASSWORD_2),
 ]
 
 
-def merge_username_passwords(args: List[Tuple[str, str]],
-            extended_args: Union[List[Union[List, Tuple, Any]], None]) -> List:
+def merge_username_passwords(
+    args: List[Tuple[str, str]],
+    extended_args: Union[List[Union[List, Tuple, Any]], None],
+) -> List:
     """
     Merge the username and password with the extended args
     if the extended args is None, return the args
