7c62f26a6c723c2c6848a7b3163aa9eecdddbc8c
Sarath <vssarathc04@gmail.com>
2024-01-02 20:21:19 +0530

    waitress for starting and stopping server

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ pytest tests
 python -m pip uninstall vtop_handler -y
 
 # start the server
-gunicorn --bind 0.0.0.0:5050 wsgi:app --reload
-
+# gunicorn --bind 0.0.0.0:5050 wsgi:app --reload
+waitress-serve --listen=127.0.0.1:5000 wsgi:app
 # recording the usage statistics
 # psrecord --plot memory.png "python main.py" 
