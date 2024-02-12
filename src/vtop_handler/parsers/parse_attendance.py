0520e76cec1c0bbea2b5f1cf5d884ab8cd89ab1b
Sarath191181208 <vssarathc04@gmail.com>
2024-02-12 22:51:50 +0530

    Parsing of attendance completed

--- src/vtop_handler/parsers/parse_attendance.py
+++ src/vtop_handler/parsers/parse_attendance.py
@@ -1,37 +1,16 @@
 import pandas as pd
-from bs4 import BeautifulSoup
-
-
 import datetime
-from typing import Dict
+from typing import Dict 
 
-def get_sub_ids(attd_html: str) -> Dict[str, str]:
+def transform_fn(course_detail: str):
     """
-        returns the list of subject ids from the attendance html
-        the subject ids are in a .btn-link element in onclick
-        Returns:
-        --- 
-            Dict[str, str]: a dictionary of slots and subject ids
-            ex:
-                {
-                    "B1+TB1+TBB1": "AP2023241000001",
-                    "B2+TB2+TBB2": "AP2023241000002",
-                }
+    Extracts the course code, course title and course type from the course detail string.
+    Returns a tuple of course code, course title and course type.
     """
-
-    # use beautiful soup to get the subject ids
-    soup = BeautifulSoup(attd_html, 'html.parser')
-    slot_subid_dict = dict()
-    for link in soup.find_all('a', {'class': 'btn-link'}):
-        on_click_str = link.get('onclick')
-        # on_click_str = javascript:processViewAttendanceDetail('AP2023241000001','B1+TB1+TBB1');
-        # filter out AP2023241000001 and B1+TB1+TBB1
-        sub_id = on_click_str.split("'")[1]
-        slot = on_click_str.split("'")[3]
-        slot_subid_dict[slot] = sub_id
-    
-    return slot_subid_dict
-
+    if course_detail.count("-") >= 3:
+        parts = course_detail.split('-')
+        return parts[0], '-'.join(parts[1:-1]), parts[-1]
+    return course_detail.split('-') 
 
 def parse_attendance(attendance_html: str) -> Dict[str, Dict] :
     """
@@ -43,28 +22,25 @@ def parse_attendance(attendance_html: str) -> Dict[str, Dict] :
     df = raw_df[0]
     # removing redundant spaces in cols
     df.columns = [' '.join(str(col).strip().split()) for col in df.columns]
-
-    slot_subid_dict = get_sub_ids(attendance_html)
-
-    # create a column for subject ids and fill it with the subject ids
-    df['Subject ID'] = df['Slot'].apply(lambda x: slot_subid_dict.get(x, None))
+    # Splitting the data seperated by - into cols CSE1005 - Software Engineering - Embedded Theory
+    df[["Course Code", "Course Title", "Course Type"]] = df["Course Detail"].apply(transform_fn).apply(pd.Series)
+    # AP2023246001037 - A1+TA1 - G14
+    df[["Subject ID", "Slot", "Room No"]] = df["Class Detail"].str.split("-", expand=True)
 
     attendace_dict = dict()
     # The last column is for credits
     for row in range(df.shape[0]-1):
-
         slot = df.iloc[row]['Slot']
 
         attendace_dict[slot] = {
             "attended" : df.iloc[row]['Attended Classes'],
             "total" : df.iloc[row]['Total Classes'],
             "percentage" : df.iloc[row]['Attendance Percentage'],
-            "faculty" : df.iloc[row]["Faculty Name"],
+            "faculty" : df.iloc[row]["Faculty Detail"],
             "courseName" : df.iloc[row]['Course Title'],
             "code" : df.iloc[row]['Course Code'],
             "type" : df.iloc[row]['Course Type'],
             "subjectId" : df.iloc[row]['Subject ID'],
             "updatedOn" : datetime.datetime.now().strftime("%c")
         }
-    # print(attendace_dict)
     return attendace_dict
