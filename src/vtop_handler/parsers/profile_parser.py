bb289953cc5bea233fae80cb5dc0d8df7e7e79a4
Sarath191181208 <vssarathc04@gmail.com>
2024-02-12 23:39:22 +0530

    Fixed parsing of profile

--- src/vtop_handler/parsers/profile_parser.py
+++ src/vtop_handler/parsers/profile_parser.py
@@ -5,7 +5,6 @@ import pandas as pd
 
 from ..utils import find_image, get_from_df, get_item, nan_to_none_in_dict
 
-
 def parse_profile(profile_html: str) -> Dict:
     img_col = BeautifulSoup(profile_html, "lxml").find(id="1a")
     base64_img = find_image(str(img_col))
@@ -16,7 +15,7 @@ def parse_profile(profile_html: str) -> Dict:
     application_number = df_personal_info.iloc[1, 1]
 
     # getting proctor info
-    proctor_moblie_number = get_from_df(df_proctor_info, 9, 1)
+    proctor_moblie_number = get_from_df(df_proctor_info, 8, 1)
 
     # Generating an API Token
     api_gen = application_number
@@ -24,17 +23,28 @@ def parse_profile(profile_html: str) -> Dict:
     temptoken = base64.b64encode(api_token)
     token = temptoken.decode("ascii")
 
+    # Getting branch and program 
+    soup = BeautifulSoup(profile_html, "lxml")
+    
+    # Gettign programme and Branch Text element
+    pbe = soup.find('label', {'for': 'branchno'})
+    program, branch = None, None
+    if pbe is not None:
+        split_str = pbe.get_text().strip().split("-")
+        program = split_str[0]
+        branch = "-".join(split_str[1:])
+    
     return nan_to_none_in_dict(
         {
-            "name": df_personal_info.iloc[2, 1],
-            "branch": df_personal_info.iloc[21, 1],
-            "program": df_personal_info.iloc[20, 1],
+            "name": df_personal_info.iloc[1, 1],
+            "branch": branch,
+            "program": program,
             "regNo": df_personal_info.iloc[18, 1],
-            "appNo": df_personal_info.iloc[1, 1],
+            "appNo": df_personal_info.iloc[0, 1],
             "school": df_personal_info.iloc[22, 1],
             "email": df_personal_info.iloc[32, 1],
-            "proctorEmail": get_from_df(df_proctor_info, 7, 1),
-            "proctorName": get_from_df(df_proctor_info, 2, 1),
+            "proctorEmail": get_from_df(df_proctor_info, 6, 1),
+            "proctorName": get_from_df(df_proctor_info, 1, 1),
             "proctorMobileNumber": proctor_moblie_number,
             "profileImageBase64": base64_img,
             "token": token,
