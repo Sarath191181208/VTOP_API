3e00761e12f3e80d1c8468e97bc7e5d5cc78fc79
Sarath191181208 <vssarathc04@gmail.com>
2024-02-12 09:18:05 +0530

    + Change of URLS updated

--- src/vtop_handler/constants.py
+++ src/vtop_handler/constants.py
@@ -1,37 +1,43 @@
-VTOP_BASE_URL = r"https://vtop2.vitap.ac.in/vtop/"
-VTOP_LOGIN_URL = r"https://vtop2.vitap.ac.in/vtop/vtopLogin"
-VTOP_DO_LOGIN_URL = r"https://vtop2.vitap.ac.in/vtop/doLogin"
-VTOP_ATTENDANCE_URL = r"https://vtop2.vitap.ac.in/vtop/processViewStudentAttendance"
+VTOP_BASE_URL = r"https://vtop.vitap.ac.in/vtop/"
+VTOP_LOGIN_URL = r"https://vtop.vitap.ac.in/vtop/vtopLogin"
+
+VTOP_BASE_URL = r"https://vtop.vitap.ac.in/vtop/open/page"
+VTOP_PRE_LOGIN = r"https://vtop.vitap.ac.in/vtop/prelogin/setup"
+VTOP_LOGIN_PAGE_REDIRECT = r"https://vtop.vitap.ac.in/vtop/init/page"
+VTOP_LOGIN_URL = r"https://vtop.vitap.ac.in/vtop/login"
+VTOP_DO_LOGIN_URL = r"https://vtop.vitap.ac.in/vtop/doLogin"
+
+VTOP_ATTENDANCE_URL = r"https://vtop.vitap.ac.in/vtop/processViewStudentAttendance"
 VTOP_SINGLE_SUBJECT_ATTENDANCE_URL = (
-    r"https://vtop2.vitap.ac.in/vtop/processViewAttendanceDetail"
+    r"https://vtop.vitap.ac.in/vtop/processViewAttendanceDetail"
 )
-VTOP_TIMETABLE_URL = r"https://vtop2.vitap.ac.in/vtop/processViewTimeTable"
+VTOP_TIMETABLE_URL = r"https://vtop.vitap.ac.in/vtop/processViewTimeTable"
 VTOP_ACADHISTORY_URL = (
-    r"https://vtop2.vitap.ac.in/vtop/examinations/examGradeView/StudentGradeHistory"
+    r"https://vtop.vitap.ac.in/vtop/examinations/examGradeView/StudentGradeHistory"
 )
 VTOP_PROFILE_URL = (
-    r"https://vtop2.vitap.ac.in/vtop/studentsRecord/StudentProfileAllView"
+    r"https://vtop.vitap.ac.in/vtop/studentsRecord/StudentProfileAllView"
 )
-VTOP_MARKS_URL = r"https://vtop2.vitap.ac.in/vtop/examinations/doStudentMarkView"
+VTOP_MARKS_URL = r"https://vtop.vitap.ac.in/vtop/examinations/doStudentMarkView"
 VTOP_EXAM_SCHEDULE_URL = (
-    r"https://vtop2.vitap.ac.in/vtop/examinations/doSearchExamScheduleForStudent"
+    r"https://vtop.vitap.ac.in/vtop/examinations/doSearchExamScheduleForStudent"
 )
 VTOP_FACULTY_URL = r"https://vitap.ac.in/faculty/"
 VTOP_ACAD_CALENDER_URL = r"https://vitap.ac.in/academic-calendar/"
 
 # --- Course page ---
-COURSE_PAGE_URL = r"https://vtop2.vitap.ac.in/vtop/academics/common/StudentCoursePage"  # for gettting into the page returns-> sem id
-COURSE_PAGE_SEMESTER_URL = r"https://vtop2.vitap.ac.in/vtop/getCourseForCoursePage"  # for selecting a semester ex: WIN SEM  returns-> subject ids
-COURSE_PAGE_SELECT_COURSE_URL = r"https://vtop2.vitap.ac.in/vtop/getSlotIdForCoursePage"  # for selecting a course ex: CSE1001 python returns-> table of contents
+COURSE_PAGE_URL = r"https://vtop.vitap.ac.in/vtop/academics/common/StudentCoursePage"  # for gettting into the page returns-> sem id
+COURSE_PAGE_SEMESTER_URL = r"https://vtop.vitap.ac.in/vtop/getCourseForCoursePage"  # for selecting a semester ex: WIN SEM  returns-> subject ids
+COURSE_PAGE_SELECT_COURSE_URL = r"https://vtop.vitap.ac.in/vtop/getSlotIdForCoursePage"  # for selecting a course ex: CSE1001 python returns-> table of contents
 COURSE_PAGE_GET_CONTENT_URL = (
-    r"https://vtop2.vitap.ac.in/vtop/processViewStudentCourseDetail"
+    r"https://vtop.vitap.ac.in/vtop/processViewStudentCourseDetail"
 )
 
 # Marks View
-MARKS_VIEW_PAGE = r"https://vtop2.vitap.ac.in/vtop/examinations/doStudentMarkView"
+MARKS_VIEW_PAGE = r"https://vtop.vitap.ac.in/vtop/examinations/doStudentMarkView"
 
 # Curriculum URL
-CURRICULUM_PAGE_URL = "https://vtop2.vitap.ac.in/vtop/academics/common/Curriculum"
+CURRICULUM_PAGE_URL = "https://vtop.vitap.ac.in/vtop/academics/common/Curriculum"
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
