3ce8a0f9e3ee4beefe83dfd334337ecc03e826ef
Sarath191181208 <vssarathc04@gmail.com>
2024-02-12 09:17:41 +0530

    +Invalid CRSF Token from server exception

--- src/vtop_handler/Exceptions/__init__.py
+++ src/vtop_handler/Exceptions/__init__.py
@@ -2,3 +2,4 @@ from .captcha_failure import CaptchaFailure
 from .invalid_credentials import InvalidCredentialsException
 from .bad_request import BadRequestException
 from .custom_base_exception import CustomBaseException
+from .invalid_crsf_token import InvalidCRSFToken 
